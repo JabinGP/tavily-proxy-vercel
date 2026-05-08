40486c8791074bce7f9800370aecc2aed4c94ef3
JabinGP <1748511181@qq.com>
2026-05-08 23:49:06 +0800

    fix

--- app/app.py
+++ app/app.py
@@ -7,35 +7,32 @@ app = FastAPI(debug=True)
 
 @app.api_route("/{path:path}", methods=["GET", "POST", "DELETE", "PUT", "PATCH"])
 async def proxy(request: Request, path):
-    if not path.startswith("v"):
-        return Response("Welcome to OpenAI Proxy")
-    else:
-        headers = dict(request.headers)
-        auth_key = headers.get("authorization")
-        if not auth_key:
-            raise HTTPException(status_code=401, detail="Authorization key is required")
-        target_url = f"https://app.tavily.com/{path}"
-        async with aiohttp.ClientSession() as session:
-            try:
-                async with session.request(
-                        method=request.method,
-                        url=target_url,
-                        headers={
-                            "Authorization": headers.get("authorization"),
-                            "Content-Type": "application/json"
-                        },
-                        data=await request.body() if request.method != "GET" else None,
-                ) as response:
-                    response_content = await response.read()
-                    log(response_content)
-                    return Response(
-                        content=response_content,
-                        status_code=response.status,
-                        # headers=dict(response.headers)
-                    )
-            except Exception as e:
-                logger.error(f"Error during forwarding request: {e.__str__()}")
-                return Response("Internal server error", status_code=500)
+    headers = dict(request.headers)
+    auth_key = headers.get("authorization")
+    if not auth_key:
+        raise HTTPException(status_code=401, detail="Authorization key is required")
+    target_url = f"https://app.tavily.com/{path}"
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.request(
+                    method=request.method,
+                    url=target_url,
+                    headers={
+                        "Authorization": headers.get("authorization"),
+                        "Content-Type": "application/json"
+                    },
+                    data=await request.body() if request.method != "GET" else None,
+            ) as response:
+                response_content = await response.read()
+                log(response_content)
+                return Response(
+                    content=response_content,
+                    status_code=response.status,
+                    # headers=dict(response.headers)
+                )
+        except Exception as e:
+            logger.error(f"Error during forwarding request: {e.__str__()}")
+            return Response("Internal server error", status_code=500)
 
 
 def log(bytes_string: bytes) -> None:
